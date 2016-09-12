9f8b12cb49079c013199925af5c69bc984982041
chitra1996 <chitrangna.pallavi.singh@gmail.com>
2016-09-12 15:03:15 +0530

    before adding the content.
    cards have been added. waiting for the content to be given.

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,1 @@
-<input type="button1" onclick="location.href='http://google.com';" value="Go to Google" />
-<input type="button2" onclick="location.href='http://google.com';" value="Go to Google" />
-<input type="button3" onclick="location.href='http://google.com';" value="Go to Google" />
-<input type="button4" onclick="location.href='http://google.com';" value="Go to Google" />
+
